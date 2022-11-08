fa050c6c08a8df0d81367d78525c1a21a8b1c8d3
Laxus66 <Laxus2301@gmail.com>
2022-11-08 21:34:23 +0700

    edit MVC

--- Main/index.php
+++ Main/index.php
@@ -1,9 +1,5 @@
 <?php
     include "header.php";
-    include "slide.php";
     include "main.php";
-    include "intro.php";
-    include "blog.php";
-    include "sale.php";
     include "footer.php";
 ?>
