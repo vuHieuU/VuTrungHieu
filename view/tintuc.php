46681303b59b7c59f4c3936c02c00c60bb552892
Phamhung144 <117258497+Phamhung144@users.noreply.github.com>
2022-11-13 21:06:51 +0700

    Add files via upload

---
+++ view/tintuc.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/tintuc.css">
+</head>
+<body>
+  <?php include "header.php"?>
+    <div class="container">
+<!-- Slide -->
+<?php
+  // include "slide.php"
+?>
+<div class="tintuc">
+  <div class="tintuc-left">
+      <h1 class="tintuc-title">Tin tức</h1>
+      <div class="tintuc-left1">
+      <div class="list-blog-box">
+          <div class="tintuc-img">
+            <img src="./Images/img/img_raucuqua/tintuc/tintuc03.jpg" alt="">
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
+      </div>
+      
+       <div class="list-blog-box">
+          <div class="tintuc-img">
+            <img src="./Images/img/img_raucuqua/tintuc/tintuc03.jpg" alt="">
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
+      </div>
+      <div class="list-blog-box">
+          <div class="tintuc-img">
+            <img src="./Images/img/img_raucuqua/tintuc/tintuc03.jpg" alt="">
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
+      </div>
+      <div class="list-blog-box">
+          <div class="tintuc-img">
+            <img src="./Images/img/img_raucuqua/tintuc/tintuc03.jpg" alt="">
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
+      </div>
+      <div class="list-blog-box">
+          <div class="tintuc-img">
+            <img src="./Images/img/img_raucuqua/tintuc/tintuc03.jpg" alt="">
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
+      </div>
+      <div class="list-blog-box">
+          <div class="tintuc-img">
+            <img src="./Images/img/img_raucuqua/tintuc/tintuc03.jpg" alt="">
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
+      </div>
+      <div class="list-blog-box">
+          <div class="tintuc-img">
+            <img src="./Images/img/img_raucuqua/tintuc/tintuc03.jpg" alt="">
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
+      </div>
+      <div class="list-blog-box">
+          <div class="tintuc-img">
+            <img src="./Images/img/img_raucuqua/tintuc/tintuc03.jpg" alt="">
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
+      </div>
+      <div class="list-blog-box">
+          <div class="tintuc-img">
+            <img src="./Images/img/img_raucuqua/tintuc/tintuc03.jpg" alt="">
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
+      </div>
+
+      </div>
+  </div>
+
+    <!-- product category -->
+    <div class="tintuc-right">
+    <div class="product-category1 mgb">
+              <h3>Bài Viết Liên Quan</h3>
+              <ul>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
+                <li><i class="fa-solid fa-angles-right"></i><a style="font-weight: bold;" href="#">Xem Tất Cả</a></li>
+                <div class="search">
+                  <input type="text" name="" id="" placeholder="Tìm kiếm bài viết liên quan...">
+                  <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+              </ul>
+              
+          </div>
+    
+          <div class="product-category1">
+              <h3>Bài Viết Mới Nhất</h3>
+              <ul>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
+                <li><i class="fa-solid fa-angles-right"></i><a style="font-weight: bold;" href="#">Xem Tất Cả</a></li>
+                <div class="search">
+                  <input type="text" name="" id="" placeholder="Tìm kiếm bài viết liên quan...">
+                  <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+              </ul>
+              
+          </div>
+    </div>
+          
+          
+          
+</div>
+
+  <?php include "intro.php"?>
+  <?php //  include "blog.php"?>
+  <?php include "footer.php"?>
+</div>
+  </body>
+  </html>
