edbdc908d66912fb7903dc6122b317550d184316
Fh nhạc <109519655+haiddph@users.noreply.github.com>
2022-11-18 09:56:39 +0700

    contact and folder quanlydonhang

--- view/contact.php
+++ view/contact.php
@@ -5,38 +5,51 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Contact</title>
     <link rel="stylesheet" href="../Main/css/contact.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer"
+    />
+   
 </head>
 
 <body>
-    <?php
-     include "header.php";
-    ?>
-        <div class="contact">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d878.7157625705689!2d105.74652633382482!3d21.037981729085836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b991d80fd5%3A0x53cefc99d6b0bf6f!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYw!5e0!3m2!1svi!2s!4v1667964574364!5m2!1svi!2s"
-                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-                    </iframe>
-                    <div class="left-col-content">
-                        <h1>Contact us</h1>
-                        <p>Bạn đang tìm những sản phẩm đủ tiêu chuẩn 3 không-VietGap hãy liên hệ với chúng thôi để biết thêm nhiều chi tiết liên quan nhé ! </p>
-                        <form id="contact-form" method="post">
-                            <label for="name">Full name</label><br>
-                            <input type="text" id="name" name="name" placeholder="Your Full Name" required>
-                            <label for="email">Email Address</label>
-                            <input type="email" id="email" name="email" placeholder="Your Email Address" required><br>
-                            <label for="message">Message</label>
-                            <textarea rows="6" placeholder="Your Message" id="message" name="message" required></textarea><br>
-                            <!--<a href="javascript:void(0)">--><button type="submit" id="submit" name="submit">Send</button>
-                            <!--</a>-->
-                        </form>
-                        <div id="error"></div>
-                        <div id="success-msg"></div>
-                    </div>
-                    </div>
-    <?php
-        // include "footer.php";
-    ?>
+<?php
+include 'header.php';
+?>
+
+    <div class="contacts">
+
+        <div class="contact-detail">
+
+            <div class="contact-ggmap">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d878.7157625705689!2d105.74652633382482!3d21.037981729085836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b991d80fd5%3A0x53cefc99d6b0bf6f!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYw!5e0!3m2!1svi!2s!4v1667964574364!5m2!1svi!2s"
+                    width="800" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="contact-ggmap-text">
+                    <h1></h1>
+                    <p></p>
+
+                </div>
+            </div>
+            <div class="contact-right">
+
+                <h1>Contact Us</h1>
+                <p>Sản phẩm chuẩn VietGap an toàn với người tiêu dùng có thắc mắc ,hay nhu cầu tìm hiểu thêm về sản phẩm xin vui lòng để lại thông tin liên hệ để được hỗ trợ nhanh nhất - Xin trân thành cảm ơn</p>
+                <input type="text" id="User" name="User" placeholder="Họ Tên Của Bạn" required>
+                <input type="text" id="email" name="email" placeholder="Gmail-Email Của Bạn" required>
+                <textarea rows="7" id="messege" placeholder="Tin nhắn của bạn" cols="" required></textarea>
+                <input type="button" onclick="myFunction()" value="Submit ">
+            </div>
+
+        </div>
+        <div class="contact-blog">
+            <a href="http://"> <i class="fa-brands fa-facebook"></i></a>
+            <a href="http://"> <i class="fa-brands fa-twitter"></i></a>
+            <a href="http://"> <i class="fa-brands fa-google"></i></a>
+            <a href="http://"> <i class="fa-brands fa-telegram"></i></a>
+            <i class="fa-sharp fa-solid fa-phone">+0965545200</i>
+        </div>
+    </div>
+<?php  include 'footer.php ';?>
 </body>
 
 </html>
