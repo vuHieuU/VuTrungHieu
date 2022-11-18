c1b4bfce0c8d0abb22ed03ae45b6f2c92c7f209a
Fh nhạc <109519655+haiddph@users.noreply.github.com>
2022-11-18 13:35:59 +0700

    edit-contact.php

--- view/contact.php
+++ view/contact.php
@@ -7,13 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact</title>
     <link rel="stylesheet" href="../Main/css/contact.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer"
-    />
-   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 
 <body>
-<?php
+    <?php
 include 'header.php';
 ?>
 
@@ -22,8 +23,10 @@ include 'header.php';
         <div class="contact-detail">
 
             <div class="contact-ggmap">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d878.7157625705689!2d105.74652633382482!3d21.037981729085836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b991d80fd5%3A0x53cefc99d6b0bf6f!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYw!5e0!3m2!1svi!2s!4v1667964574364!5m2!1svi!2s"
-                    width="800" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d878 25705689!2d105.74652633382482!3d21.037981729085836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b991d80fd5%3A0x53cefc99d6b0bf6f!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYw!5e0!3m2!1svi!2s!4v1667964574364!5m2!1svi!2s"
+                    width="800" height="400" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
                 <div class="contact-ggmap-text">
                     <h1></h1>
                     <p></p>
@@ -33,11 +36,12 @@ include 'header.php';
             <div class="contact-right">
 
                 <h1>Contact Us</h1>
-                <p>Sản phẩm chuẩn VietGap an toàn với người tiêu dùng có thắc mắc ,hay nhu cầu tìm hiểu thêm về sản phẩm xin vui lòng để lại thông tin liên hệ để được hỗ trợ nhanh nhất - Xin trân thành cảm ơn</p>
+                <p>Sản phẩm chuẩn VietGap an toàn với người tiêu dùng có thắc mắc ,hay nhu cầu tìm hiểu thêm về sản phẩm
+                    xin vui lòng để lại thông tin liên hệ để được hỗ trợ nhanh nhất - Xin trân thành cảm ơn</p>
                 <input type="text" id="User" name="User" placeholder="Họ Tên Của Bạn" required>
-                <input type="text" id="email" name="email" placeholder="Gmail-Email Của Bạn" required>
+                <input type="text" id="emails" name="email" placeholder="Gmail-Email Của Bạn" required>
                 <textarea rows="7" id="messege" placeholder="Tin nhắn của bạn" cols="" required></textarea>
-                <input type="button" onclick="myFunction()" value="Submit ">
+                <input id="button-input" type="submit" onclick="myFunction()" value="Submit ">
             </div>
 
         </div>
@@ -49,7 +53,7 @@ include 'header.php';
             <i class="fa-sharp fa-solid fa-phone">+0965545200</i>
         </div>
     </div>
-<?php  include 'footer.php ';?>
+    <?php  include 'footer.php ';?>
 </body>
 
 </html>
