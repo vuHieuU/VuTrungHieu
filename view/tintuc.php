93561b0355b1df66db9e0be6243fadda9ab24717
Phamhung144 <117258497+Phamhung144@users.noreply.github.com>
2022-11-21 04:28:25 +0700

    sua trang tin tuc

--- view/tintuc.php
+++ view/tintuc.php
@@ -17,75 +17,46 @@
 ?>
 <div class="tintuc">
   <div class="tintuc-left">
-      <h1 class="tintuc-title">Tin tức</h1>
-      <div class="tintuc-left1">
-      <div class="list-blog-box">
-          <div class="tintuc-img">
-            <img src="../Images/tintuc/tintuc.jpg" alt="">
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
-      </div>
+      <!-- Slideshow container -->
       
-       <div class="list-blog-box">
-          <div class="tintuc-img">
-            <img src="../Images/tintuc/tintuc.jpg" alt="">
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
-      </div>
-      <div class="list-blog-box">
-          <div class="tintuc-img">
-            <img src="../Images/tintuc/tintuc.jpg" alt="">
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
-      </div>
-      <div class="list-blog-box">
-          <div class="tintuc-img">
-            <img src="../Images/tintuc/tintuc.jpg" alt="">
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
-      </div>
-      <div class="list-blog-box">
-          <div class="tintuc-img">
-            <img src="../Images/tintuc/tintuc.jpg" alt="">
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
-      </div>
-      <div class="list-blog-box">
-          <div class="tintuc-img">
-            <img src="../Images/tintuc/tintuc.jpg" alt="">
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
-      </div>
-      <div class="list-blog-box">
-          <div class="tintuc-img">
-            <img src="../Images/tintuc/tintuc.jpg" alt="">
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
-      </div>
-      <div class="list-blog-box">
-          <div class="tintuc-img">
-            <img src="../Images/tintuc/tintuc.jpg" alt="">
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
-      </div>
-      <div class="list-blog-box">
-          <div class="tintuc-img">
-            <img src="../Images/tintuc/tintuc.jpg" alt="">
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime, dolor soluta corporis velit voluptas maiores debitis numquam dolorum. Blanditiis quo doloremque repudiandae iusto optio, ullam consequuntur!</p>
-      </div>
+<div class="slideshow-container">
 
-      </div>
+<!-- Full-width images with number and caption text -->
+<div class="mySlides fade">
+  <img src="../Images/slideshow/slideshow.jpg" style="width:100%">
+</div>
+
+<div class="mySlides fade">
+  <img src="../Images/slideshow/slideshow2.jpg" style="width:100%">
+</div>
+
+<div class="mySlides fade">
+  <img src="../Images/slideshow/slideshow3.jpg" style="width:100%">
+</div>
+
+<!-- Next and previous buttons -->
+<!-- <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a> -->
+</div>
+<br>
+
+<!-- The dots/circles -->
+<!-- <div style="text-align:center">
+<span class="dot" onclick="currentSlide(1)"></span>
+<span class="dot" onclick="currentSlide(2)"></span>
+<span class="dot" onclick="currentSlide(3)"></span>
+</div> -->
+  
+
+
   </div>
 
     <!-- product category -->
     <div class="tintuc-right">
     <div class="product-category1 mgb">
-              <h3>Bài Viết Liên Quan</h3>
+              <h3>Tin nổi bật</h3>
               <ul>
-                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
-                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
-                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
+              <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
                 <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
                 <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
                 <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
@@ -99,31 +70,141 @@
               
           </div>
     
-          <div class="product-category1">
-              <h3>Bài Viết Mới Nhất</h3>
-              <ul>
-                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
-                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
-                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
-                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
-                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
-                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
-                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ sáng nay, giờ đến cái nịt cũng chả còn.</a></li>
-                <li><i class="fa-solid fa-angles-right"></i><a style="font-weight: bold;" href="#">Xem Tất Cả</a></li>
-                <div class="search">
-                  <input type="text" name="" id="" placeholder="Tìm kiếm bài viết liên quan...">
-                  <button><i class="fa-solid fa-magnifying-glass"></i></button>
-                </div>
-              </ul>
-              
-          </div>
+         
     </div>
+
+    
           
           
           
 </div>
-  <?php //  include "blog.php"?>
-  <?php include "footer.php"?>
+    
+      <div class="list-status">
+        
+        <div class="status-left">
+        <h1 class="gioithieu-title">Tin sức khỏe - Mẹo vặt</h1>
+        <div class="list-status-left">
+        
+        <div class="list-blog-box">
+          <div>
+          <div class="tintuc-img">
+            <img src="../Images/tintuc/tintuc.jpg" alt="">
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime!</p>
+          </div>
+        </div>
+
+        <div class="list-blog-box">
+        <div>
+          <div class="tintuc-img">
+            <img src="../Images/tintuc/tintuc.jpg" alt="">
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime!</p>
+          </div>
+        </div>
+
+        <div class="list-blog-box">
+        <div>
+          <div class="tintuc-img">
+            <img src="../Images/tintuc/tintuc.jpg" alt="">
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime!</p>
+          </div>
+        </div>
+
+        <div class="list-blog-box">
+        <div>
+          <div class="tintuc-img">
+            <img src="../Images/tintuc/tintuc.jpg" alt="">
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nemo a sint maxime!</p>
+          </div>
+        </div>
+
+        
+
+        
+      </div>
+        </div>
+
+        <div class="list-status-right">
+        <div class="product-category2 mgb">
+              <h3>Danh mục tin tức</h3>
+              <ul>
+              <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
+                <li><i class="fa-sharp fa-solid fa-chevron-right"></i></i><a href="#">Hot hot hot: Rau củ quả nhập từ hôm qua, hôm nay hết sạch còn cái nịt.</a></li>
+              </ul>
+              <!-- Slideshow container -->
+      
+<div class="slideshow-container">
+
+<!-- Full-width images with number and caption text -->
+<div class="mySlides1 fade position-tintic">
+  <img src="../Images/sanpham/sanphamok/anhdoc3.jpg" style="width:100%">
 </div>
+
+<div class="mySlides1 fade">
+  <img src="../Images/sanpham/sanphamok/anhdoc4.jpg" style="width:100%">
+</div>
+
+<div class="mySlides1 fade">
+  <img src="../Images/sanpham/sanphamok/anhdoc5.jpg" style="width:100%">
+</div>
+
+<!-- Next and previous buttons -->
+<!-- <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a> -->
+</div>
+<br>
+
+<!-- The dots/circles -->
+<!-- <div style="text-align:center">
+<span class="dot" onclick="currentSlide(1)"></span>
+<span class="dot" onclick="currentSlide(2)"></span>
+<span class="dot" onclick="currentSlide(3)"></span>
+</div> -->
+  
+          </div>
+        </div>
+      </div>
+
+  
+</div>
+
+<script>
+  let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
+
+<script>
+   let slideIndex1 = 0;
+showSlides1();
+
+function showSlides1() {
+  let i;
+  let slides1 = document.getElementsByClassName("mySlides1");
+  for (i = 0; i < slides1.length; i++) {
+    slides1[i].style.display = "none";
+  }
+  slideIndex1++;
+  if (slideIndex1 > slides1.length) {slideIndex1 = 1}
+  slides1[slideIndex1-1].style.display = "block";
+  setTimeout(showSlides1, 5000); // Change image every 2 seconds
+}
+  </script>
+
   </body>
   </html>
